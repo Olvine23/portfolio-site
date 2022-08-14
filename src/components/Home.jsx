b28374765d11e7d1613b1b193195c970f8580ffb
Olvine23 <olivinegeorge@gmail.com>
2022-08-14 13:57:54 +0300

    Added fade features

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
-
+import { Reveal } from "react-reveal";
 
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <Reveal effect="fadeInUp">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#28c7fa]">Hi, my name is</p>
@@ -29,6 +30,7 @@ const Home = () => {
           </button>
         </div>
       </div>
+      </Reveal>
     </div>
   );
 };
