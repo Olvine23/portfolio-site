0e63e5bab941828b517695ab76d1ff8f462be511
Olvine23 <olivinegeorge@gmail.com>
2022-08-18 16:11:16 +0300

    added gh-pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import me from "../assets/me.jpg";
+ 
 
 const About = () => {
   return (
     <div name="about" className="w-full sm:h-screen bg-[#0a192f] text-gray-300">
+
       <div className="flex flex-col justify-center items-center w-full h-90">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
@@ -22,6 +24,7 @@ const About = () => {
             <p className="mt-10 text-[#8892b0]">Olvine here 👋. <br/> These are some of the interesting facts about me.</p>
           </div>
           <div>
+            
             <p>
               I am passionate about building excellent software that improves
               the lives of those around me. I specialize in creating software
@@ -33,11 +36,11 @@ const About = () => {
             <br />
 
             <p>
-              I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?
+            Experienced Software Developer adept in bringing forth expertise in design, installation, testing and
+            maintenance of software systems. Equipped with a diverse and promising skill-set. Proficient in
+            various platforms, languages, and embedded systems. Experienced with the latest cutting edge
+            development tools and procedures. Able to effectively self-manage during independent projects, as
+            well as collaborate as part of a productive team.
             </p>
 
             <br />
